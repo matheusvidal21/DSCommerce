067094a1658c9e3adce87db20d0844ab7e49c0c6
matheusvidal21 <matheusvidal140@gmail.com>
2024-01-25 12:35:31 -0300

    Equals hashCode

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -86,5 +87,20 @@ public class Order {
 
     public List<Product> getProduct(){
         return items.stream().map(x -> x.getProduct()).toList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
--- src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
@@ -5,6 +5,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class OrderItemPK implements Serializable {
@@ -34,5 +35,23 @@ public class OrderItemPK implements Serializable {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItemPK that = (OrderItemPK) o;
+
+        if (!Objects.equals(order, that.order)) return false;
+        return Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = order != null ? order.hashCode() : 0;
+        result = 31 * result + (product != null ? product.hashCode() : 0);
+        return result;
     }
 }
